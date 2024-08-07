ffff2dc4aa8fa62e4eda3b98ac772d8003bc582e
pinch <pinchasmt@gmail.com>
2024-08-07 17:48:15 +0300

    addedmodels

--- AirDefense/Models/Country.cs
+++ AirDefense/Models/Country.cs
@@ -1,6 +1,22 @@
-﻿namespace AirDefense.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AirDefense.Models
 {
-    public class Countries
+    public class Country
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name= "Country Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Country distance")]
+        public int Distance {  get; set; }
+        public Country()
+        {
+        }
     }
+
 }
